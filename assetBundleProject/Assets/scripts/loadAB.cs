2a84150ec38355d8471bc0bce5a83ebf7282a353
JoyGitHubPlace <1175725736@qq.com>
2021-04-13 00:08:54 +0800

    readme.txt v0.1

---
+++ assetBundleProject/Assets/scripts/loadAB.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.Networking;
+
+public class loadAB : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("assetBundleRequest");
+    }
+    void loadABlocal() {
+        AssetBundle ab = AssetBundle.LoadFromFile("AssetBundles/aa.bb");
+        GameObject obj = ab.LoadAsset<GameObject>("Capsule");
+        Instantiate(obj);
+    }
+
+    void loadABFromMemory() {
+        AssetBundle ab = AssetBundle.LoadFromMemory(File.ReadAllBytes("AssetBundles/aa.bb"));
+        GameObject obj = ab.LoadAsset<GameObject>("Capsule");
+        Instantiate(obj);
+    }
+
+    IEnumerator assetBundleRequest() {
+        AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(File.ReadAllBytes("AssetBundles/aa.bb"));
+        yield return request;
+        AssetBundle ab = request.assetBundle;
+        GameObject obj = ab.LoadAsset<GameObject>("Capsule");
+        Instantiate(obj);
+    }
+
+    IEnumerator unityWebRequest()
+    {
+        string url = @"127.0.0.1/AssetBundles/AssetBundles/aa.bb";
+        UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(url);
+        yield return request.SendWebRequest();
+        AssetBundle ab = (request.downloadHandler as DownloadHandlerAssetBundle).assetBundle;
+        GameObject obj = ab.LoadAsset<GameObject>("Capsule");
+        Instantiate(obj);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+assetBundle system
+
+v0.1
+
+1 base version   
